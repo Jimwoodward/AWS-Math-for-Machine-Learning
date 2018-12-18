40bd4a1b73f77ddc97b0f9d9d8fafec59b92ee64
Jim Woodward <jim.woodward@snagajob.com>
2018-12-18 14:17:05 -0500

    Update with more debugging/print statements

--- question1.py
+++ question1.py
@@ -9,28 +9,33 @@ fmin = [49, 62, 66, 69, 61] # First question about going from theory to code is 
 fmax = [71, 75, 75, 81, 80]
 
 # Print for debug
-print(fmin)
-print(fmax, '\n')
+print('Fmin array: ', fmin)
+print('Fmax array: ', fmax, '\n')
 
 # Use numpy to turn the arrays into vectors
 fmin_vec = np.array(fmin)
 fmax_vec = np.array(fmax)
 
 # Print for debug
-print(fmin_vec)
-print(fmax_vec, '\n')
+print('Fmin vector: ', fmin_vec)
+print('Fmax vector: ', fmax_vec, '\n')
 
 # Subtract the min temps vector from the max temps vector to create the delta vector
 fdelta = fmax_vec - fmin_vec
 
 # Print the answer
-print(fdelta, '\n')
+print('The answer is: ')
+print('Fdelta: ', fdelta, '\n')
 
 
 ################################################################################################################################################################################
 ################################################################################################################################################################################
 
 # Bonus work
+
+print('####################################################################')
+print('####################################################################')
+print('Bonus work', '\n')
 
 # 1. What if we treated those 1xn vectors as 1xn matrices?
 
@@ -39,14 +44,15 @@ fmin_matr = np.matrix(fmin)
 fmax_matr = np.matrix(fmax)
 
 # Print for debug
-print(fmin_matr)
-print(fmax_matr, '\n')
+print('Fmin matrix: ', fmin_matr)
+print('Fmax matrix: ', fmax_matr, '\n')
 
 # Matrix subtraction
 fdelta = fmax_matr - fmin_matr
 
 # Print the answer
-print(fdelta, '\n')
+print('The answer is: ')
+print('Fdelta: ', fdelta, '\n')
 
 # 2. What if we transposed the original vector/matrix data object so that it was nx1 instead of 1xn (or so the vector/matrix as Python knows it is a column vector instead of a row vector)? This is how the first question is actually set up.
 
@@ -57,14 +63,15 @@ fmin_vec = np.transpose(np.array(fmin))
 fmax_vec = np.transpose(np.array(fmax))
 
 # Print for debug
-print(fmin_vec)
-print(fmax_vec,'\n')
+print('Fmin vector: ', fmin_vec)
+print('Fmax matrix: ', fmax_vec, '\n')
 
 # Vector subtraction
 fdelta = fmax_vec - fmin_vec
 
 # Print the answer
-print(fdelta, '\n')
+print('The answer is: ')
+print('Fdelta: ', fdelta, '\n')
 
 # 2.b. Attempt with matrices
 
@@ -73,11 +80,12 @@ fmin_vec = np.transpose(np.matrix(fmin))
 fmax_vec = np.transpose(np.matrix(fmax))
 
 # Print for debug
-print(fmin_vec)
-print(fmax_vec,'\n')
+print('Fmin matrix: ', fmin_vec)
+print('Fmax matrix: ', fmax_vec, '\n')
 
 # Matrix subtraction
 fdelta = fmax_vec - fmin_vec
 
 # Print the answer
-print(fdelta, '\n')
+print('The answer is: ')
+print('Fdelta: ', fdelta, '\n')
